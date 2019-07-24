efbba2e1dc5312a8a4efcb61573c34e8818ea810
betofigueiredo <herberto_f@yahoo.com.br>
2019-07-24 20:13:16 -0300

    Clear redux store

--- src/store/libraries/store.js
+++ src/store/libraries/store.js
@@ -3,15 +3,21 @@ const libraries = {
 	by_id: {},
 	all_ids: [],
 	profile_preview_id: 0,
-	search: {
-		address: '',
-		lat: 0,
-		lng: 0,
-		radius: 5,
-		book_author: '',
-		library: '',
-		lfl: 0,
+	preview: {
+		open: 'library-info-window',
+		name: '',
+		url: '',
+		number_of_books: 0,
 	},
+	// search: {
+	// 	address: '',
+	// 	lat: 0,
+	// 	lng: 0,
+	// 	radius: 5,
+	// 	book_author: '',
+	// 	library: '',
+	// 	lfl: 0,
+	// },
 };
 
 export default libraries;
--- src/store/map/store.js
+++ src/store/map/store.js
@@ -1,11 +1,11 @@
 const map = {
 	zoom: 13,
-	radius: 5,
-	lat: -23.991506,
-	lng: -46.258046,
-	address: '',
-	book_author: '',
-	library: '',
+	// radius: 5,
+	// lat: -23.991506,
+	// lng: -46.258046,
+	// address: '',
+	// book_author: '',
+	// library: '',
 	preview: {
 		open: 'library-info-window',
 		name: '',
