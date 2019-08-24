ccb0483aac87f7281cd1ea0617a13f56a3691c0a
betofigueiredo <herberto_f@yahoo.com.br>
2019-08-24 14:47:51 -0300

    Page 404 transform from class to hooks

--- src/pages/NotFound404/NotFound404.js
+++ src/pages/NotFound404/NotFound404.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 // CSS
 import CSSModules from 'react-css-modules';
 import styles from './style.module.scss';
 
-class NotFound404 extends Component {
-	static getDerivedStateFromProps(nextProps) {
-		const { match, history } = nextProps;
-
+function NotFound404({ match, history }) {
+	useEffect(() => {
 		switch (match.url) {
 		case '/login':
 		case '/sign':
@@ -23,28 +21,58 @@ class NotFound404 extends Component {
 			break;
 		default:
 		}
+	}, []);
 
-		return {};
-	}
-
-	state = {};
-
-	render() {
-		return (
-			<div className="container">
-				<div className="row">
-					<div className="col-xs-12">
-						<p styleName="teste">404</p>
-					</div>
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-xs-12">
+					<p styleName="teste">404</p>
 				</div>
 			</div>
-		);
-	}
+		</div>
+	);
 }
+// class NotFound404 extends Component {
+// 	static getDerivedStateFromProps(nextProps) {
+// 		const { match, history } = nextProps;
+
+// 		switch (match.url) {
+// 		case '/login':
+// 		case '/sign':
+// 		case '/signin':
+// 		case '/entrar':
+// 			history.push('/sign/in');
+// 			break;
+// 		case '/signout':
+// 		case '/logout':
+// 		case '/logoff':
+// 			history.push('/sign/out');
+// 			break;
+// 		default:
+// 		}
+
+// 		return {};
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div className="container">
+// 				<div className="row">
+// 					<div className="col-xs-12">
+// 						<p styleName="teste">404</p>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		);
+// 	}
+// }
 
 NotFound404.propTypes = {
 	match: PropTypes.object.isRequired,
-	history: PropTypes.object.isRequired,
+	history: PropTypes.shape({
+		push: PropTypes.func.isRequired,
+	}).isRequired,
 };
 
 export default CSSModules(NotFound404, styles, { allowMultiple: true });
