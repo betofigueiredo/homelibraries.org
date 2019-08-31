55edc91e75d35fb3b6c6900a5e72d36e745a4071
betofigueiredo <herberto_f@yahoo.com.br>
2019-08-31 00:09:07 -0300

    Replace react-loadable for react suspense

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Loadable from 'react-loadable';
 
 import LoadingPage from './components/_loadings/LoadingPage/LoadingPage';
 
@@ -9,63 +8,36 @@ import NotFound404 from './pages/NotFound404';
 import SignOut from './pages/SignOut';
 import SignIn from './pages/SignIn';
 
-const loading = () => <LoadingPage />;
-
-const Home = Loadable({
-	loader: () => import('./pages/Home'),
-	loading,
-});
-const Libraries = Loadable({
-	loader: () => import('./pages/Libraries'),
-	loading,
-});
-const MyBooks = Loadable({
-	loader: () => import('./pages/MyBooks'),
-	loading,
-});
-const Messages = Loadable({
-	loader: () => import('./pages/Messages'),
-	loading,
-});
-const Account = Loadable({
-	loader: () => import('./pages/Account/Account'),
-	loading,
-});
-const AccountName = Loadable({
-	loader: () => import('./pages/Account/AccountName'),
-	loading,
-});
-const AccountUrl = Loadable({
-	loader: () => import('./pages/Account/AccountUrl'),
-	loading,
-});
-const AccountAddress = Loadable({
-	loader: () => import('./pages/Account/AccountAddress'),
-	loading,
-});
-const AccountMap = Loadable({
-	loader: () => import('./pages/Account/AccountMap'),
-	loading,
-});
+const Home = lazy(() => import('./pages/Home'));
+const Libraries = lazy(() => import('./pages/Libraries'));
+const MyBooks = lazy(() => import('./pages/MyBooks'));
+const Messages = lazy(() => import('./pages/Messages'));
+const Account = lazy(() => import('./pages/Account/Account'));
+const AccountName = lazy(() => import('./pages/Account/AccountName'));
+const AccountUrl = lazy(() => import('./pages/Account/AccountUrl'));
+const AccountAddress = lazy(() => import('./pages/Account/AccountAddress'));
+const AccountMap = lazy(() => import('./pages/Account/AccountMap'));
 
 const Routes = () => (
-	<Switch>
-		<Route path="/libraries" exact component={Libraries} />
-		<Route path="/mybooks" exact component={MyBooks} />
-		<Route path="/messages" exact component={Messages} />
-		<Route path="/messages/:uuid" exact component={Messages} />
-		<Route path="/account" exact component={Account} />
-		<Route path="/account/name" exact component={AccountName} />
-		<Route path="/account/url" exact component={AccountUrl} />
-		<Route path="/account/address" exact component={AccountAddress} />
-		<Route path="/account/map" exact component={AccountMap} />
-		<Route path="/sign/in" exact component={SignIn} />
-		<Route path="/sign/out" exact component={SignOut} />
+	<Suspense fallback={<LoadingPage />}>
+		<Switch>
+			<Route path="/libraries" exact component={Libraries} />
+			<Route path="/mybooks" exact component={MyBooks} />
+			<Route path="/messages" exact component={Messages} />
+			<Route path="/messages/view/:uuid" exact component={Messages} />
+			<Route path="/account" exact component={Account} />
+			<Route path="/account/name" exact component={AccountName} />
+			<Route path="/account/url" exact component={AccountUrl} />
+			<Route path="/account/address" exact component={AccountAddress} />
+			<Route path="/account/map" exact component={AccountMap} />
+			<Route path="/sign/in" exact component={SignIn} />
+			<Route path="/sign/out" exact component={SignOut} />
 
-		{/* 404 e signout */}
-		<Route path="/" exact component={Home} />
-		<Route path="*" exact component={NotFound404} />
-	</Switch>
+			{/* 404 e signout */}
+			<Route path="/" exact component={Home} />
+			<Route path="*" exact component={NotFound404} />
+		</Switch>
+	</Suspense>
 );
 
 export default Routes;
